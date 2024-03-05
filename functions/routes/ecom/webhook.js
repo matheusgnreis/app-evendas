6a49553450a6664bdc84bdcafd050f4854905a35
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-03-05 11:19:18 -0300

    chore(webhook): send carts for collection to send to evendas

--- functions/routes/ecom/webhook.js
+++ functions/routes/ecom/webhook.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const { firestore } = require('firebase-admin')
 
 // read configured E-Com Plus app data
 const getAppData = require('./../../lib/store-api/get-app-data')
@@ -54,7 +55,20 @@ exports.post = ({ appSdk }, req, res) => {
                         customer = response.data
                       }
                     } else {
-                      return res.sendStatus(501)
+                      const documentRef = firestore().doc(`cart/${storeId}`)
+                      await documentRef.set({
+                        data: {
+                          storeId,
+                          trigger,
+                          [resource.slice(0, -1)]: cart,
+                          customer
+                        },
+                        updatedAt: firestore.Timestamp.fromDate(new Date())
+                      })
+                      return res.send({
+                        status: 400,
+                        text: 'Waiting to send'
+                      })
                     }
                   } else {
                     return res.sendStatus(204)
